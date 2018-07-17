2afb9420f5f26117ca098bf9bc949884f7251e12
Trojain <550392236@qq.com>
2018-07-17 16:33:06 +0800

    路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,38 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/pages/home/Home'
-import City from '@/pages/city/City'
-import Detail from '@/pages/detail/Detail'
-import Movie from '@/pages/movie/Movie'
-import MovieDetail from '@/pages/movie/MovieDetail'
+
+// import Home from '@/pages/home/Home'
+// import City from '@/pages/city/City'
+// import Detail from '@/pages/detail/Detail'
+// import Movie from '@/pages/movie/Movie'
+// import MovieDetail from '@/pages/movie/MovieDetail'
+
+// vue路由懒加载  异步加载
+const Home = (resolve) => {
+	import ('@/pages/home/Home').then((module) => {
+		resolve(module)
+	})
+}
+const City = (resolve) => {
+	import ('@/pages/city/City').then((module) => {
+		resolve(module)
+	})
+}
+const Detail = (resolve) => {
+	import ('@/pages/detail/Detail').then((module) => {
+		resolve(module)
+	})
+}
+const Movie = (resolve) => {
+	import ('@/pages/movie/Movie').then((module) => {
+		resolve(module)
+	})
+}
+const MovieDetail = (resolve) => {
+	import ('@/pages/movie/MovieDetail').then((module) => {
+		resolve(module)
+	})
+}
 
 Vue.use(Router)
 
